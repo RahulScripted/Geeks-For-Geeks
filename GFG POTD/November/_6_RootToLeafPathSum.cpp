9779cb37e8e007e03e9277283059f454845dac28
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-11-06 10:48:42 +0530

    Target Complete

---
+++ GFG POTD/November/_6_RootToLeafPathSum.cpp
@@ -0,0 +1,113 @@
+// Given a binary tree, where every node value is a number. Find the sum of all the numbers that are formed from root to leaf paths. The formation of the numbers would be like 10*parent + current (see the examples for more clarification).
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    vector<string> ip;
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+class Solution {
+  public:
+    int dfs(Node* node, int currentSum) {
+        if (!node) return 0;
+
+        currentSum = currentSum * 10 + node->data;
+        if (!node->left && !node->right) return currentSum;
+
+        int leftSum = dfs(node->left, currentSum);
+        int rightSum = dfs(node->right, currentSum);
+        return leftSum + rightSum;
+    }
+    
+    int treePathsSum(Node *root) {
+        return dfs(root, 0);
+    }
+};
+
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution ob;
+        cout << ob.treePathsSum(root);
+        cout << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
