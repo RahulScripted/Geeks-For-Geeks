672cc92920c35ffce7f44b11e2849e73d4cbe608
Rahul Goswami <goswami.rahul1002@gmail.com>
2025-02-07 07:22:35 +0530

    Done

---
+++ GFG POTD/2025/February/_7_InorderTraversal.cpp
@@ -0,0 +1,92 @@
+// Given a Binary Tree, your task is to return its In-Order Traversal. An inorder traversal first visits the left child (including its entire subtree), then visits the node, and finally visits the right child (including its entire subtree).
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+class Solution {
+  public:
+    void pushNode(Node* node,vector<int>& ans){
+        if(!node) return;
+        pushNode(node -> left,ans);
+        ans.push_back(node -> data);
+        pushNode(node -> right,ans);
+    }
+    
+    vector<int> inOrder(Node* root) {
+        vector<int>ans;
+        pushNode(root,ans);
+        return ans;
+    }
+};
+
+Node* buildTree(string str) {
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    vector<string> ip;
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    Node* root = new Node(stoi(ip[0]));
+    queue<Node*> queue;
+    queue.push(root);
+
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+        Node* currNode = queue.front();
+        queue.pop();
+        string currVal = ip[i];
+
+        if (currVal != "N") {
+            currNode->left = new Node(stoi(currVal));
+            queue.push(currNode->left);
+        }
+
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        if (currVal != "N") {
+            currNode->right = new Node(stoi(currVal));
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+
+        Solution ob;
+        vector<int> res = ob.inOrder(root);
+        for (int i = 0; i < res.size(); i++)
+            cout << res[i] << " ";
+        cout << endl;
+        cout << "~" << "\n";
+    }
+}
